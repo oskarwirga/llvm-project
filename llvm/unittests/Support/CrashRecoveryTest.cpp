ead8b530ab850a6cf1cf12bf2378792b1b79fef9
Aleksei.Glushko <aleksei.glushko@jetbrains.com>
2025-04-08 16:05:37 +0200

    Mute SEGFAULT recovery tests.
        Several tests in CrashRecoveryTest become invalid after
        the commit of "Disable signal handling of JVM GC signals".

--- llvm/unittests/Support/CrashRecoveryTest.cpp
+++ llvm/unittests/Support/CrashRecoveryTest.cpp
@@ -39,7 +39,12 @@ static void incrementGlobal() { ++GlobalInt; }
 static void llvmTrap() { LLVM_BUILTIN_TRAP; }
 static void incrementGlobalWithParam(void *) { ++GlobalInt; }
 
+#define SKIP_WITH_KOTLIN_PATCH() \
+  GTEST_SKIP() << "Crash recovery from SEGFAULT disabled by Kotlin patch"
+
 TEST(CrashRecoveryTest, Basic) {
+  SKIP_WITH_KOTLIN_PATCH();
+
   llvm::CrashRecoveryContext::Enable();
   GlobalInt = 0;
   EXPECT_TRUE(CrashRecoveryContext().RunSafely(incrementGlobal));
@@ -57,6 +62,8 @@ struct IncrementGlobalCleanup : CrashRecoveryContextCleanup {
 static void noop() {}
 
 TEST(CrashRecoveryTest, Cleanup) {
+  SKIP_WITH_KOTLIN_PATCH();
+
   llvm::CrashRecoveryContext::Enable();
   GlobalInt = 0;
   {
@@ -77,6 +84,8 @@ TEST(CrashRecoveryTest, Cleanup) {
 }
 
 TEST(CrashRecoveryTest, DumpStackCleanup) {
+  SKIP_WITH_KOTLIN_PATCH();
+
   SmallString<128> Filename;
   std::error_code EC = sys::fs::createTemporaryFile("crash", "test", Filename);
   EXPECT_FALSE(EC);
